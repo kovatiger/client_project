477a4cc86504624683e70039b4890b9b43f0790c
nastya <nastya.gorlachyova@mail.ru>
2022-03-04 03:18:58 +0300

    add js to auth

---
+++ src/main/webapp/script.js
@@ -0,0 +1,49 @@
+let signUp = document.querySelector('.sign-up')
+let signUpForm = document.querySelector('.sign-up-form')
+let signInForm = document.querySelector('.sign-in-form')
+let signIn = document.querySelector('.sign-in')
+let stripe = document.querySelector('.stripe')
+let divIn = document.querySelector('.div-in')
+let divUp = document.querySelector('.div-up')
+
+
+signUp.addEventListener('click', () =>{
+    console.log(signUpForm);
+
+    signUpForm.style.display = 'block';
+    signInForm.style.display = 'none';
+    divIn.classList.toggle('stripe')
+    divUp.classList.toggle('stripe')
+})
+signIn.addEventListener('click',() =>{
+    signUpForm.style.display = 'none';
+    signInForm.style.display = 'block';
+    divIn.classList.toggle('stripe')
+    divUp.classList.toggle('stripe')
+})
+
+window.addEventListener('load', function () {
+    function sendData () {
+        const XHR = new XMLHttpRequest();
+
+        const FD = new FormData ( form );
+
+        XHR.addEventListener('load', function (event) {
+            alert(event.target.responseText);
+        });
+
+        XHR.addEventListener('error', function (event) {
+            alert("OOPS! SOMETHING WENT WRONG!");
+        });
+
+        XHR.open('POST', 'http://localhost:8081/authorization');
+
+        XHR.send(FD);
+    }
+
+    const form = document.querySelector('form')
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        sendData();
+    });
+})
